b93ed29cc44c6f7e4054335ec2c1d95fc142f9a9
Omega <56974140+0xm3ga@users.noreply.github.com>
2023-07-29 23:54:40 +0100

    Separated pbs from media cloudfront (#16)
    * changed retriever to redirect method
    * add cloudfront for s3

--- aws/api/v1/src/lambdas/retrieve_media_function/app.py
+++ aws/api/v1/src/lambdas/retrieve_media_function/app.py
@@ -1,80 +1,102 @@
-# import os
-import base64
+import logging
+import os
 
 import boto3
-from botocore.exceptions import NoCredentialsError
+from botocore.exceptions import ClientError, NoCredentialsError
+
+# Set up logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def object_exists(bucket: str, key: str):
+    logger.info(f"object_exists: {bucket} {key}")
+    s3 = boto3.client("s3")
+    try:
+        s3.head_object(Bucket=bucket, Key=key)
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "404":
+            return False
+        else:
+            raise
+    else:
+        return True
+
+
+def process_media(filename: str, size: str, extension: str) -> str:
+    username = "username"
+    file_type = "videos"
+
+    key = f"{username}/{file_type}/{size}"
+    return key
 
 
 def lambda_handler(event, context):
-    s3 = boto3.client("s3")
+    logger.info(event)
     try:
-        bucket_name = "media.bluecollarverse.com-processed"  # os.environ["PROCESSED_MEDIA_BUCKET"]
+        raw_media_bucket = os.environ["RAW_MEDIA_BUCKET"]
+        processed_media_bucket = os.environ["PROCESSED_MEDIA_BUCKET"]
+        media_domain_name = os.environ["MEDIA_DOMAIN_NAME"]
+
         filename = event["pathParameters"]["filename"]  # fetch filename from the url path parameter
 
         queryStringParameters = event.get("queryStringParameters") or {}
         size = queryStringParameters.get("size", "medium")
         extension = queryStringParameters.get("extension", "jpg")
 
+        # Make sure to use jpg instead of jpeg
+        if extension == "jpeg":
+            extension = "jpg"
+
         # List of accepted sizes
         accepted_sizes = ["small", "medium", "large"]
 
         # List of accepted formats
-        accepted_formats = ["jpg", "jpeg", "png", "gif"]
+        accepted_formats = ["jpg", "jpeg", "png", "gif", "mp4"]
 
         if size not in accepted_sizes:
             size = "medium"
 
-        # Check if the provided format is in the list of accepted formats. If not, default to 'jpg'.
+        # Check if the provided format is in the list of accepted formats
         if extension not in accepted_formats:
-            extension = "jpg"
+            return {"statusCode": 400, "body": f"Extension {extension} is not supported."}
+
     except Exception as e:
+        logger.error(f"Preprocessing error: {str(e)}")
         return {"statusCode": 500, "body": f"Preprocessing error: {str(e)}"}
 
     try:
         # Construct the key
         key = f"{filename}/{size}.{extension}"
+        logger.info(key)
 
-        # OPTION 1
-        # # Generate signed URL
-        # url = f"https://{bucket_name}.s3.amazonaws.com/{key}"
+        # Check that the object exists in processed bucket
+        if not object_exists(processed_media_bucket, key):
+            logger.info(f"Object {key} not found in processed bucket. Checking raw bucket...")
+            # Check that the original object exists in raw bucket
+            if not object_exists(raw_media_bucket, key):
+                logger.warning(f"Object {key} not found in raw bucket.")
+                return {"statusCode": 404, "body": f"Object not found: {str(key)}"}
 
-        # # Return a 302 redirect to the signed URL
-        # return {
-        #     "statusCode": 302,
-        #     "headers": {
-        #         "Location": url,
-        #     },
-        # }
+            # TODO: call processing lambda (for just the specified size and ext)
+            try:
+                key = process_media(filename, size, extension)
+            except Exception:
+                logger.error("Error processing the file.")
+                return {"statusCode": 500, "body": "Error processing the file."}
 
-        # OPTION 2:
-        # Download the image from S3
-        file_object = s3.get_object(Bucket=bucket_name, Key=key)
-        file_content = file_object["Body"].read()
+        url = f"https://{media_domain_name}/{key}"
 
-        # Convert the image to base64
-        base64_image = base64.b64encode(file_content).decode("utf-8")
-
-        # Determine the content type for the response
-        if extension == "jpg" or extension == "jpeg":
-            content_type = "image/jpeg"
-        elif extension == "png":
-            content_type = "image/png"
-        elif extension == "gif":
-            content_type = "image/gif"
-        else:
-            content_type = "image/jpeg"
-
-        # Return the image as the response
         return {
-            "statusCode": 200,
+            "statusCode": 302,
             "headers": {
-                "Content-Type": content_type,
+                "Location": url,
             },
-            "body": base64_image,
-            "isBase64Encoded": True,
         }
 
     except NoCredentialsError:
+        logger.error("No AWS credentials found")
         return {"statusCode": 403, "body": "No AWS credentials found"}
     except Exception as e:
+        logger.error(str(e))
         return {"statusCode": 500, "body": str(e)}
