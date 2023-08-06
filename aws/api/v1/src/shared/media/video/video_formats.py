02a605b11a626617751446902945d30a3aadcaf6
Omega <56974140+0xm3ga@users.noreply.github.com>
2023-08-06 22:10:55 +0100

    refactored media formats (#34)

--- aws/api/v1/src/shared/media/video/video_formats.py
+++ aws/api/v1/src/shared/media/video/video_formats.py
@@ -1,31 +1,34 @@
 from dataclasses import dataclass
 
-from shared.media.base import BaseMediaFormats, MediaType
+from shared.media.base import BaseMediaFormats, Extension, MediaFormatUtils, MediaType
 from shared.media.constants import FORMATS
 
 
 class VideoFormats(BaseMediaFormats):
-    MEDIA_TYPE = MediaType.VIDEO
-    FORMATS = FORMATS[MEDIA_TYPE]
-
-    @classmethod
-    def get_media_type(cls) -> MediaType:
-        if not cls.MEDIA_TYPE:
-            raise
-        return cls.MEDIA_TYPE
+    media_type = MediaType.VIDEO
+    formats = FORMATS[MediaType.VIDEO]
 
 
 @dataclass
 class VideoFormat:
-    format: str
+    FORMAT_STR = "VideoFormat(extension={extension}, content_type={content_type})"
 
-    @property
-    def media_type(self):
-        return VideoFormats.get_media_type().value
-
-    @property
-    def extension(self):
-        return self.format
+    extension: Extension
+    content_type: str
+    media_type = MediaType.VIDEO
 
     def __str__(self):
-        return f"VideoFormat(format={self.format}, media_type={self.media_type})"
+        return self.FORMAT_STR.format(extension=self.extension, content_type=self.content_type)
+
+    @classmethod
+    def from_extension(cls, ext: str):
+        MediaFormatUtils.is_extension_allowed(extension=ext, media_type=cls.media_type)
+        extension = Extension[ext.upper()]
+        return cls(extension=extension, content_type=f"{cls.media_type.value}/{extension.value}")
+
+    @classmethod
+    def from_content_type(cls, content_type: str):
+        _, ext = content_type.split("/")
+        MediaFormatUtils.is_extension_allowed(extension=ext, media_type=cls.media_type)
+        extension = Extension[ext.upper()]
+        return cls(extension=extension, content_type=f"{cls.media_type.value}/{extension.value}")
